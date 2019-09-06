35616e95b8d0240c2faa567610a4a5a3c2b302e3
Jim Kratus <jimkratus@gmail.com>
2019-09-06 15:56:32 -0500

    transitions working

--- d3-playground/src/components/transitions/LearnBarChart/index.jsx
+++ d3-playground/src/components/transitions/LearnBarChart/index.jsx
@@ -8,39 +8,39 @@ function rando() {
 }
 
 const data = [
-  { name: 'A', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'B', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'C', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'E', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'D', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'F', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'G', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'H', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'I', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'J', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'K', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'L', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'M', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'N', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'O', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'P', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'Q', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'R', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'S', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'T', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'U', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'V', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'W', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'X', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'Y', a: rando(), b: rando(), c: rando(), d: rando() },
-  { name: 'Z', a: rando(), b: rando(), c: rando(), d: rando() },
+  { name: 'A', a: 1, b: 26, c: rando(), d: rando() },
+  { name: 'B', a: 2, b: 24, c: rando(), d: rando() },
+  { name: 'C', a: 3, b: 25, c: rando(), d: rando() },
+  { name: 'E', a: 4, b: 23, c: rando(), d: rando() },
+  { name: 'D', a: 51, b: 22, c: rando(), d: rando() },
+  { name: 'F', a: 6, b: 51, c: rando(), d: rando() },
+  { name: 'G', a: 7, b: 20, c: rando(), d: rando() },
+  { name: 'H', a: 8, b: 19, c: rando(), d: rando() },
+  { name: 'I', a: 9, b: 18, c: rando(), d: rando() },
+  { name: 'J', a: 11, b: 17, c: rando(), d: rando() },
+  { name: 'K', a: 12, b: 16, c: rando(), d: rando() },
+  { name: 'L', a: 13, b: 15, c: rando(), d: rando() },
+  { name: 'M', a: 14, b: 14, c: rando(), d: rando() },
+  { name: 'N', a: 15, b: 13, c: rando(), d: rando() },
+  { name: 'O', a: 16, b: 12, c: rando(), d: rando() },
+  { name: 'P', a: 17, b: 11, c: rando(), d: rando() },
+  { name: 'Q', a: 18, b: 10, c: rando(), d: rando() },
+  { name: 'R', a: 19, b: 9, c: rando(), d: rando() },
+  { name: 'S', a: 21, b: 8, c: rando(), d: rando() },
+  { name: 'T', a: 22, b: 7, c: rando(), d: rando() },
+  { name: 'U', a: 24, b: 6, c: rando(), d: rando() },
+  { name: 'V', a: 23, b: 5, c: rando(), d: rando() },
+  { name: 'W', a: 52, b: 50, c: rando(), d: rando() },
+  { name: 'X', a: 26, b: 3, c: rando(), d: rando() },
+  { name: 'Y', a: 27, b: 2, c: rando(), d: rando() },
+  { name: 'Z', a: 28, b: 1, c: rando(), d: rando() },
 ];
 
 const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 let chart, menu;
-const t = d3.transition().duration(750);
+
 const letters = data;
 const options = d3.keys(letters[0]).filter(key => key !== 'name');
 
@@ -50,14 +50,11 @@ const x = d3
   .paddingInner(0.1)
   .paddingOuter(0.1);
 
-const y = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, d => d.a)])
-  .range([height, 0]);
+const y = d3.scaleLinear().range([height, 0]);
 
 const xAxis = d3.axisBottom(x);
 
-const yAxis = d3.axisLeft(y).ticks(10);
+const yAxis = d3.axisLeft(y);
 
 const LearnBarChart = () => {
   useEffect(() => {
@@ -104,12 +101,15 @@ const LearnBarChart = () => {
   }
 
   function redraw() {
+    const t = d3.transition().duration(1500);
     const option1 = menu.property('value');
     const top = letters.sort((a, b) => b[option1] - a[option1]).slice(0, 10);
 
     x.domain(top.map(d => d.name));
+    y.domain([0, d3.max(top, d => d[option1])]);
+    yAxis.ticks(10);
 
-    let bar = chart.selectAll('.bar').data(top, d => d);
+    let bar = chart.selectAll('.bar').data(top, d => d.name);
 
     bar.join(
       enter =>
@@ -117,8 +117,8 @@ const LearnBarChart = () => {
           .append('rect')
           .attr('class', 'bar')
           .attr('x', 0)
-          .attr('y', d => y(d.a))
-          .attr('height', d => height - y(d.a))
+          .attr('y', d => y(d[option1]))
+          .attr('height', d => height - y(d[option1]))
           .attr('width', x.bandwidth())
           .style('fill-opacity', 0)
           .call(enter =>
@@ -128,13 +128,20 @@ const LearnBarChart = () => {
               .style('fill-opacity', 1)
           ),
       update =>
-        update.call(update => update.transition(t).attr('x', d => x(d.name))),
+        update.call(update =>
+          update
+            .transition(t)
+            .attr('y', d => y(d[option1]))
+            .attr('height', d => height - y(d[option1]))
+            .attr('x', d => x(d.name))
+        ),
       exit =>
         exit.call(exit =>
           exit
             .transition(t)
             .style('fill-opacity', 0)
             .attr('x', 960 + margin.left + margin.right)
+            .remove()
         )
     );
 
@@ -142,8 +149,9 @@ const LearnBarChart = () => {
       .select('.x.axis')
       .call(xAxis);
 
-    //   .attr('transform', d => `translate(0,${y(d.State) + height})`)
-    //   .style('fill-opacity', 0);
+    d3.transition(chart)
+      .select('.y.axis')
+      .call(yAxis);
   }
 
   return (
